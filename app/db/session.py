db160119437dbfb646801eb4855dea31d26c1828
Sakhile L. Ndlazi <sakhilelindah@gmail.com>
2025-02-15 17:19:36 +0200

    fix(database): Remove echo when creating tables.

--- app/db/session.py
+++ app/db/session.py
@@ -2,7 +2,7 @@ from sqlmodel import create_engine, Session
 from app.core.config import settings
 
 
-engine = create_engine(settings.database_url, echo=True)
+engine = create_engine(settings.database_url)
 
 def get_session() -> Session:
     with Session(engine) as session:
