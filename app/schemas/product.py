b85ed864dd045120c673592bb89d9d5ad4b6e554
Sakhile L. Ndlazi <sakhilelindah@gmail.com>
2025-02-15 03:42:26 +0200

    Create Pydantic 'product' model for request and response.

---
+++ app/schemas/product.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+from typing import Optional
+
+from pydantic import BaseModel
+
+class Product(BaseModel):
+    id: Optional[int] = None
+
+class ProductCreate(BaseModel):
+    name: str
+    description: str
+    price: float
+
+class ProductUpdate(BaseModel):
+    name: Optional[str] = None
+    description: Optional[str] = None
+    price: Optional[float] = None
+    
+    class Config:
+        orm_mode = True
+
+class ProductRead(BaseModel):
+    id: Optional[int] = None
+    name: str
+    description: str
+    price: float
+
+    class Config:
+        orm_mode = True
