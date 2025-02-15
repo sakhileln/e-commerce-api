09d73f56929085fd8c645f27dca28c46e55770bc
Sakhile L. Ndlazi <sakhilelindah@gmail.com>
2025-02-15 17:20:28 +0200

    fix(database): No more DB recreation.

--- app/main.py
+++ app/main.py
@@ -1,31 +1,19 @@
-from databases import Database
 from fastapi import FastAPI
+from sqlmodel import SQLModel
 
 from app.routes import users, products, orders
 from app.core.config import settings
+from app.db.session import engine
 
 app = FastAPI(title="E-Commerce API", version="0.1.1")
 
-# Initialize database connection using the correct URL
-database = Database(settings.database_url)
+# Ensure tables are created only once (persistent)
+SQLModel.metadata.create_all(engine)
 
-# Include routers from the routes folder
+# Include routers
 app.include_router(users.router, prefix="/users", tags=["Users"])
 app.include_router(products.router, prefix="/products", tags=["Products"])
 app.include_router(orders.router, prefix="/orders", tags=["Orders"])
-
-
-# Define startup and shutdown events
-@app.on_event("startup")
-async def startup():
-    # connect to database. intitalize the cache
-    await database.connect()
-
-@app.on_event("shutdown")
-async def shutdown():
-    # clean up resources
-    await database.disconnect()
-
 
 if __name__ == "__main__":
     import uvicorn
