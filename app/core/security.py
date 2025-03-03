39f6d1228b6826009d95fae9dadef0a67789d4e3
Sakhile L. Ndlazi <sakhilelindah@gmail.com>
2025-03-03 16:59:19 +0200

    feat(signup): Decode and verify access token.

--- app/core/security.py
+++ app/core/security.py
@@ -4,11 +4,15 @@ from os import getenv
 from passlib.context import CryptContext
 from jose import jwt, JWTError
 from dotenv import load_dotenv
+from fastapi import HTTPException, Depends, status
+from fastapi.security import OAuth2PasswordBearer
+
+from app.core.config import settings
+
 
 load_dotenv()
-
-
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
@@ -26,3 +30,21 @@ def create_access_token(data:dict, expires_delta:timedelta | None = None) -> str
     encoded_jwt = jwt.encode(to_encode, getenv("SECRET_KEY"), algorithm=getenv("ALGORITHM"))
     return encoded_jwt
 
+def verify_access_token(token: str) -> None:
+    """Decodes and verifies the JWT token."""
+    try:
+        payload = jwt.decode(token, settings.secret, algorithms=settings.algorithm)
+        username: str = payload.get("sub")
+        if username is None:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Invalid token",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
+        return username
+    except JWTError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid token",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
