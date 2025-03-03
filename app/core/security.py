606c47d8b1b128fe64f3c9268db86fe62cbba83a
Sakhile L. Ndlazi <sakhilelindah@gmail.com>
2025-03-03 16:14:56 +0200

    feat(signup): Utilify function for handling JWT authentication.

--- app/core/security.py
+++ app/core/security.py
@@ -1,10 +1,28 @@
+from datetime import datetime, timedelta
+from os import getenv
+
 from passlib.context import CryptContext
+from jose import jwt, JWTError
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-def verify_password(plain_password, hashed_password):
+def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
 
-def get_password_hash(password):
+def get_password_hash(password: str) -> str:
     return pwd_context.hash(password)
+
+def create_access_token(data:dict, expires_delta:timedelta | None = None) -> str:
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, getenv("SECRET_KEY"), algorithm=getenv("ALGORITHM"))
+    return encoded_jwt
+
